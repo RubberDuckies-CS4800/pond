394457601928e1b557c3ad340aff83449ec90674
matthewkhle <mattmattmattle@gmail.com>
2021-09-17 22:46:54 -0700

    added routes
    - simba route and home
    - commented vue index.html route

---
+++ back-end/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+// listen for requests
+app.listen(8000);
+
+// example layout for GET
+// app.get('/path', (req, res) => {
+// })
+
+app.get("/", (req, res) => {
+
+  // throws "ForbiddenError"
+  // res.sendFile("../front-end/public/index.html", { root: __dirname });
+  res.sendFile("./views/index.html", { root: __dirname });
+  console.log("home page accessed");
+});
+
+app.get("/simba", (req, res) => {
+  res.sendFile("./views/simba.html", { root: __dirname });
+  console.log("i love simba!");
+});
