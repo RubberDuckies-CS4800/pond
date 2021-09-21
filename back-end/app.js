4a8da090b9522242895779c9d65021d968ae58e4
matthewkhle <mattmattmattle@gmail.com>
2021-09-20 19:14:01 -0700

    Added lodash package

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,5 @@
-// To run, use "nodemon app" in the terminal
+// To run, use "nodemon app" in the terminal (might have to do npm install nodemon)
+// Ctrl + c to stop the app
 
 const express = require("express");
 
@@ -12,10 +13,9 @@ app.listen(8000);
 // })
 
 app.get("/", (req, res) => {
-
   // throws "ForbiddenError"
-    // res.sendFile("../front-end/public/index.html", { root: __dirname });
-  
+  // res.sendFile("../front-end/public/index.html", { root: __dirname });
+
   res.sendFile("./views/index.html", { root: __dirname });
   console.log("home page accessed");
 });
