ec6be8509abd989c3ba7002d47ea2d4e6fbd2594
bobby-shaj <57640405+bobby-shaj@users.noreply.github.com>
2021-09-23 16:40:27 -0700

    9/23  4:40 pm

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,8 @@
-// To run, use "nodemon app" in the terminal (might have to do npm install nodemon)
-// Ctrl + c to stop the app
+// To run, use "nodemon app" in the terminal
 
 const express = require("express");
 
-// Lodash for A4
-const _ = require("lodash");
-
-
 const app = express();
-
 
 // listen for requests
 app.listen(8000);
@@ -18,6 +12,7 @@ app.listen(8000);
 // })
 
 
+//display current time
 app.get("/time", (req, res) => {
   var today = new Date();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -28,9 +23,10 @@ app.get("/time", (req, res) => {
 
 
 app.get("/", (req, res) => {
-  // throws "ForbiddenError"
-  // res.sendFile("../front-end/public/index.html", { root: __dirname });
 
+  // throws "ForbiddenError"
+    // res.sendFile("../front-end/public/index.html", { root: __dirname });
+  
   res.sendFile("./views/index.html", { root: __dirname });
   console.log("home page accessed");
 });
@@ -38,37 +34,4 @@ app.get("/", (req, res) => {
 app.get("/simba", (req, res) => {
   res.sendFile("./views/simba.html", { root: __dirname });
   console.log("i love simba!");
-
-  // Lodash test code
-  const names = ['gus', 'x']
-  const puppies = _.concat(names, 'appa', 4);
-  console.log(puppies);
 });
-
-app.get("/pixar", (req, res) => {
-  console.log(req);
-  res.sendFile("./views/pixar.html", { root: __dirname });
-});
-
-app.get("/pixar_characters", (req, res) => {
-  console.log(req);
-  res.sendFile("./assets/characters.json", { root: __dirname });
-});
-
-function computeFibb(index) {
-  if (index == 1) {
-    return [1, 1]
-  } else {
-    const prev = computeFibb(index - 1)
-    return [prev[1], prev[0] + prev[1]]
-  }
-}
-
-app.get("/fibbonacci/:index", (req, res) => {
-  const idx = parseInt(req.params['index'])
-  if (isNaN(idx) || idx < 1) {
-    res.status(400).send('index must be a number >= 1')
-    return
-  }
-  res.status(200).send('' + computeFibb(idx)[1])
-})
