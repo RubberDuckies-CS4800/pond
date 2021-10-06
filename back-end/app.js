fb186567426ebc176ffb3eeaf542c5958453fdd2
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-10-06 09:32:34 -0700

    Handle SPA

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 // To run, use "nodemon app" in the terminal
 
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +10,12 @@ const app = express();
 // })
 
 app.use(express.static('../front-end/dist/'));
+
+// Serve additional pages in the app by returning the compiled app. Vue compiles
+// all pages to a single html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('../front-end/dist/index.html'))
+})
 
 // listen for requests
 app.listen(8000);
