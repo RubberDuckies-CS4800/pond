f1a75f60806788526de097621c3d20df82133e32
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-10-06 09:29:12 -0700

    Clean out assignment endpoints

--- back-end/app.js
+++ back-end/app.js
@@ -2,63 +2,13 @@
 
 const express = require("express");
 
-var bodyParser = require('body-parser')
-
 const app = express();
-
-// listen for requests
-app.listen(8000);
-
-app.use(bodyParser.json())
-
-var messages = [{message: 'hello it\'s A4'}]
 
 // example layout for GET
 // app.get('/path', (req, res) => {
 // })
 
-var validator = require('validator');
+app.use(express.static('../front-end/dist/'));
 
-app.get("/emails/:email", (req, res) => {
-  res.sendFile("./assets/emails.json", { root: __dirname });
-  let email = req.params.email;
-  if(validator.isEmail(email)) {
-      console.log(email + " is a valid email.");
-  } else {
-    console.log(email + " is NOT a valid email.");
-  }
-});
-
-app.get("/a4", (req, res) => {
-  res.sendFile("./views/a4.html", { root: __dirname });
-});
-
-//A4
-app.post("/a4", (req, res) => {
-  messages.push(req.body)
-  res.sendStatus(200)
-});
-
-//display current time
-app.get("/time", (req, res) => {
-  var today = new Date();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-  res.sendFile("./views/index.html", { root: __dirname });
-  console.log(time);
-});
-
-
-app.get("/", (req, res) => {
-
-  // throws "ForbiddenError"
-    // res.sendFile("../front-end/public/index.html", { root: __dirname });
-  
-  res.sendFile("./views/index.html", { root: __dirname });
-  console.log("home page accessed");
-});
-
-app.get("/simba", (req, res) => {
-  res.sendFile("./views/simba.html", { root: __dirname });
-  console.log("i love simba!");
-});
+// listen for requests
+app.listen(8000);
