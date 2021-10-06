ad83f0203565ea2cf782702eddc8012230771ddf
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-10-06 09:42:03 -0700

    Server logic

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,14 @@
 // To run, use "nodemon app" in the terminal
 
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketIo = require("socket.io");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 
 // example layout for GET
 // app.get('/path', (req, res) => {
@@ -17,5 +22,19 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve('../front-end/dist/index.html'))
 })
 
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+
+    //socket.broadcast sends a message to everyone in the room
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
 // listen for requests
-app.listen(8000);
+server.listen(8000);
+console.log("Listening on port 8000");
