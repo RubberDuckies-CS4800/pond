f0b5570ed214149ee502a8a644d3ab0d9743aba1
matthewkhle <mattmattmattle@gmail.com>
2021-10-06 00:46:39 -0700

    Edited app.js, edited file locations

--- back-end/app.js
+++ back-end/app.js
@@ -1,19 +1,35 @@
-// To run, use "nodemon app" in the terminal
-
+// set up server with express
 const express = require("express");
-
-
-
 const app = express();
 
-// listen for requests
-app.listen(8000);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
+const { v4: uuidV4 } = require("uuid");
 
-app.get("/", (req, res) => {
-  
-  // res.sendFile("./views/index.html", { root: __dirname });
+app.use(express.static("public"));
 
-  console.log("home page accessed");
+app.get("/get-room", (req, res) => {
+  res.json({ roomId: uuidV4() });
 });
 
+// room path
+app.get("/:room", (req, res) => {
+  res.json({ roomId: req.params.room });
+});
+
+// runs anytime someone connects to our webpage
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+
+    //socket.broadcast sends a message to everyone in the room
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(3000);
