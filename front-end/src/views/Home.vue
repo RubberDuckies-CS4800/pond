e4729d2a1631b773175ab1b1cc5d105b9f100899
melissaruiz1 <melissaruiz913@yahoo.com>
2021-10-06 17:03:36 -0700

    front-end updates

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <div id="stars"></div>
+  <div id="stars2"></div>
+  <div id="stars3"></div>
+  <img class="company_logo" src="https://raw.githubusercontent.com/cs480-projects/cs480-projects.github.io/master/teams-fall2021/rubber-duckies/pond-final.png" alt="Company Logo">
+  <v-container 
+    flex 
+    align="center"
+    justify="center">
+    <v-card 
+      flex
+      class="meeting_input mx-auto px-auto"
+      outlined
+  >
+    <v-text-field
+            placeholder="Meeting Code"
+            solo
+    ></v-text-field>
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        Submit
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  </v-container>
+  <!-- <div id="stars"></div>
+  <div id="stars2"></div>
+  <div id="stars3"></div> -->
+  <!-- <v-container fluid> -->
+    <!-- <img class="company_logo" src="https://raw.githubusercontent.com/cs480-projects/cs480-projects.github.io/master/teams-fall2021/rubber-duckies/pond-final.png" alt="Company Logo"> -->
+      <!-- <v-row align="auto">
+
+      </v-row>
+      <v-row align="auto">
+        <v-card></v-card>
+      </v-row> -->
+<!-- </v-container> -->
+</div>
+  <!-- <body>
+
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+      <div>
+          <img src="https://raw.githubusercontent.com/cs480-projects/cs480-projects.github.io/master/teams-fall2021/rubber-duckies/pond-final.png" alt="Company Logo">
+      </div>
+  </body> -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style scoped>
+
+.meeting_input {
+  display: inline-block;
+  width: 40%;
+}
+
+.company_logo {
+  width: 30%;
+}
+
+</style>
