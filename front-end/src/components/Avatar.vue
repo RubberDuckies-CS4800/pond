f237044395e9e040798bc5e070f9e389821d7861
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-11-22 12:17:52 -0800

    Consistent sizing

--- front-end/src/components/Avatar.vue
+++ front-end/src/components/Avatar.vue
@@ -8,7 +8,7 @@
           talking: talking && avatar.audio,
         }"
         size="120"
-        :style="`height: 90px; min-width: 90px; width: 90px; top: ${avatar.top}px; left:${avatar.left}px; position:absolute;`"
+        :style="`height: 90px; min-width: 90px; width: 90px; top: ${screenPos.y}px; left:${screenPos.x}px; position:absolute;`"
         @contextmenu.prevent="on.click"
         @mousedown="onMouseDown($event)"
         :color="`rgb(${color.r},${color.g},${color.b})`"
@@ -82,6 +82,18 @@ export default {
     scaledVolume() {
       return this.volume / 100;
     },
+    canvasPos() {
+      return {
+        x: this.avatar.left,
+        y: this.avatar.top,
+      };
+    },
+    screenPos() {
+      return this.canvasToScreenPosition({
+        x: this.avatar.left,
+        y: this.avatar.top,
+      });
+    },
   },
   methods: {
     setTalking(to) {
@@ -111,14 +123,19 @@ export default {
       e.preventDefault();
       document.onmouseup = () => this.onMouseUp();
       document.onmousemove = (e) => this.onMouseMove(e);
-      this.dragOffsetX = e.clientX - this.avatar.left;
-      this.dragOffsetY = e.clientY - this.avatar.top;
+      this.dragOffsetX = e.clientX - this.screenPos.x;
+      this.dragOffsetY = e.clientY - this.screenPos.y;
     },
     onMouseMove(e) {
       e.preventDefault();
+      let p = {
+        x: e.clientX - this.dragOffsetX,
+        y: e.clientY - this.dragOffsetY,
+      };
+      p = this.mouseToCanvasPosition(p);
       updateAvatar({
-        left: e.clientX - this.dragOffsetX,
-        top: e.clientY - this.dragOffsetY,
+        left: p.x,
+        top: p.y,
       });
     },
     onMouseUp() {
@@ -130,6 +147,59 @@ export default {
     },
     setEnableVideo(value) {
       this.enableVideo = value;
+    },
+    // Copied from Whiteboard.vue
+    mouseToCanvasPosition(point) {
+      const svgWidth = 16;
+      const svgHeight = 9;
+      const svgAspectRatio = svgWidth / svgHeight;
+      let xOrigin = 0;
+      let yOrigin = 0;
+      let scale;
+      const containerWidth = window.innerWidth;
+      const containerHeight = window.innerHeight;
+      const containerAspectRatio = containerWidth / containerHeight;
+
+      if (containerAspectRatio > svgAspectRatio) {
+        const boxWidth = svgAspectRatio * containerHeight;
+        xOrigin = (containerWidth - boxWidth) / 2;
+        scale = containerHeight / svgHeight;
+      } else {
+        const boxHeight = containerWidth / svgAspectRatio;
+        yOrigin = (containerHeight - boxHeight) / 2;
+        scale = containerWidth / svgWidth;
+      }
+
+      return {
+        x: (point.x - xOrigin) / scale,
+        y: (point.y - yOrigin) / scale,
+      };
+    },
+    canvasToScreenPosition(point) {
+      const svgWidth = 16;
+      const svgHeight = 9;
+      const svgAspectRatio = svgWidth / svgHeight;
+      let xOrigin = 0;
+      let yOrigin = 0;
+      let scale;
+      const containerWidth = window.innerWidth;
+      const containerHeight = window.innerHeight;
+      const containerAspectRatio = containerWidth / containerHeight;
+
+      if (containerAspectRatio > svgAspectRatio) {
+        const boxWidth = svgAspectRatio * containerHeight;
+        xOrigin = (containerWidth - boxWidth) / 2;
+        scale = containerHeight / svgHeight;
+      } else {
+        const boxHeight = containerWidth / svgAspectRatio;
+        yOrigin = (containerHeight - boxHeight) / 2;
+        scale = containerWidth / svgWidth;
+      }
+
+      return {
+        x: point.x * scale + xOrigin,
+        y: point.y * scale + yOrigin,
+      };
     },
   },
   updated() {
