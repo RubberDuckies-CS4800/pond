c6d85473e49472c5195a3c4fbdbc0862e07f9f50
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2021-11-25 00:13:34 -0800

    Cleaning VideoStream.vue

--- front-end/src/components/VideoStream.vue
+++ front-end/src/components/VideoStream.vue
@@ -1,74 +1,73 @@
 <template>
-  <div>
-    <video ref="video" :muted="muted" :volume="volume" />
-    <h1>volume: {{ volume }}</h1>
-  </div>
+	<div>
+		<video ref="video" :muted="muted" :volume="volume" />
+	</div>
 </template>
 
 <script>
 export default {
-  name: "VideoStream",
-  props: {
-    stream: MediaStream,
-    muted: Boolean,
-    volume: Number,
-  },
-  data() {
-    return {
-      isMounted: false,
-    };
-  },
-  methods: {
-    updateMeter() {
-      const array = new Uint8Array(this.analyser.fftSize);
-      this.analyser.getByteTimeDomainData(array);
-      let max = 0;
-      for (let a of array) {
-        a = Math.abs(a - 128);
-        max = Math.max(max, a);
-      }
-
-      this.$emit('talking', max > 4);
-
-      if (this.isMounted)
-        window.requestAnimationFrame(() => this.updateMeter());
-    },
-    updateVolume() {
-      const vid = this.$refs.video;
-      vid.volume = this.volume;
-    }
+	name: "VideoStream",
+	props: {
+		stream: MediaStream,
+		muted: Boolean,
+		volume: Number,
 	},
-  watch: {
-    volume: function() {
-      this.updateVolume();
-    }
-  },
-  mounted() {
-    this.isMounted = true;
+	data() {
+		return {
+			isMounted: false,
+		};
+	},
+	methods: {
+		updateMeter() {
+			const array = new Uint8Array(this.analyser.fftSize);
+			this.analyser.getByteTimeDomainData(array);
+			let max = 0;
+			for (let a of array) {
+				a = Math.abs(a - 128);
+				max = Math.max(max, a);
+			}
 
-    const vid = this.$refs.video;
-    vid.srcObject = this.stream;
-    vid.addEventListener("loadedmetadata", () => {
-      vid.play();
-    });
+			this.$emit("talking", max > 4);
 
-    const ctx = new AudioContext();
-    const src = ctx.createMediaStreamSource(this.stream);
-    window.src = src;
-    const analyser = ctx.createAnalyser();
-    src.connect(analyser);
+			if (this.isMounted)
+				window.requestAnimationFrame(() => this.updateMeter());
+		},
+		updateVolume() {
+			const vid = this.$refs.video;
+			vid.volume = this.volume;
+		},
+	},
+	watch: {
+		volume: function () {
+			this.updateVolume();
+		},
+	},
+	mounted() {
+		this.isMounted = true;
 
-    this.analyser = analyser;
-    this.updateMeter();
-  },
-  beforeUnmount() {
-    this.isMounted = false;
-  },
+		const vid = this.$refs.video;
+		vid.srcObject = this.stream;
+		vid.addEventListener("loadedmetadata", () => {
+			vid.play();
+		});
+
+		const ctx = new AudioContext();
+		const src = ctx.createMediaStreamSource(this.stream);
+		window.src = src;
+		const analyser = ctx.createAnalyser();
+		src.connect(analyser);
+
+		this.analyser = analyser;
+		this.updateMeter();
+	},
+	beforeUnmount() {
+		this.isMounted = false;
+	},
 };
 </script>
 <style>
 video {
-  height: 100%;
-  transform: rotateY(180deg);
+	height: 100%;
+	transform: rotateY(180deg);
 }
 </style>
